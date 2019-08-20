8cb69a0bfdd6a5cddb1a5debfa0f474e8fd14fee
FranklinDM <mrmineshafter17@gmail.com>
2019-08-20 20:30:06 +0800

    Separate Tile code file into separate files according to class

---
+++ src/TileLib/TileInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TileLib
+{
+    public class TileInfo : Attribute
+    {
+        public string Name;
+        public readonly bool hasflyout;
+        public readonly bool hasOptions;
+
+        public TileInfo(string name, bool hasflyout, bool hasOptions)
+        {
+            this.Name = name;
+            this.hasflyout = hasflyout;
+            this.hasOptions = hasOptions;
+        }
+    }
+}
