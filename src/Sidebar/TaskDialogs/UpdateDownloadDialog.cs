d20259db284148065aee93ebf245fd53384d63e6
FranklinDM <mrmineshafter17@gmail.com>
2019-08-26 19:38:12 +0800

    Refactor update dialog class - Separate update download task dialog from main update class - Remove alternative dialog (used in XP) - Localize dialog strings

---
+++ src/Sidebar/TaskDialogs/UpdateDownloadDialog.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System.Net;
+using System.IO;
+using System.Reflection;
+using Sidebar.Core;
+using System.Windows;
+using System.ComponentModel;
+
+namespace Sidebar.TaskDialogs
+{
+    public class UpdateDownloadDialog
+    {
+        private static TaskDialog td;
+
+        public static void ShowDialog()
+        {
+            td = new TaskDialog();
+            td.Caption = (string)Application.Current.TryFindResource("UpdateDownloadDialogTitle");
+            td.InstructionText = (string)Application.Current.TryFindResource("UpdateDownloadDialogHeader");
+            td.StandardButtons = TaskDialogStandardButtons.Cancel;
+
+            TaskDialogProgressBar progressBar = new TaskDialogProgressBar("progressBar");
+            progressBar.Maximum = 100;
+
+            td.Controls.Add(progressBar);
+            td.Closing += new EventHandler<TaskDialogClosingEventArgs>(tdDownload_Closing);
+
+            if (!Directory.Exists(LongBarMain.sett.path + "\\Updates"))
+            {
+                Directory.CreateDirectory(LongBarMain.sett.path + "\\Updates");
+            }
+
+            client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+            client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+            // TODO: Hardcoded string
+            client.DownloadFileAsync(new Uri("http://franklindm.github.io/AvalonBar/services/UpdatePackage.zip"), LongBarMain.sett.path + "\\Updates\\Update");
+
+            td.Show();
+        }
+
+        private static WebClient client = new WebClient();
+
+        static void tdDownload_Closing(object sender, TaskDialogClosingEventArgs e)
+        {
+            if (e.TaskDialogResult == TaskDialogResult.Cancel && client.IsBusy)
+                client.CancelAsync();
+        }
+
+        static void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Error == null && !e.Cancelled)
+            {
+                UpdateManager.UpdateFiles(LongBarMain.sett.path);
+                Application.Current.Dispatcher.Invoke((Action)delegate
+                {
+                    Application.Current.Shutdown();
+                }, null);
+                // TODO: Usage of hardcoded executable name
+                NativeMethods.ShellExecute(IntPtr.Zero, "open", Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\LongBar.exe", String.Empty, String.Empty, 1);
+                td.Close();
+            }
+        }
+
+        static void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            ((TaskDialogProgressBar)td.Controls["progressBar"]).Value = e.ProgressPercentage;
+        }
+    }
+}
